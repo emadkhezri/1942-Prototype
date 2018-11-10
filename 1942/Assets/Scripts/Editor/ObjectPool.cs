441f335b19e615b054f04eb009f1985521f0daae
Emad Khezri <emad.khezri@gmail.com>
2018-11-10 17:49:40 +1100

    Generic ObjectPool Class has been added.

---
+++ 1942/Assets/Scripts/Editor/ObjectPool.cs
@@ -0,0 +1,83 @@
+﻿namespace com.emad.game
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>
+    /// Signleton Generic object pool of Unity components
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ObjectPool<T> where T : Component
+    {
+        private const int DEFAULT_POOL_SIZE = 10;
+        /// <summary>
+        /// Number of objects in the pool
+        /// <Remark>It needs to be set before instantiation the pool</Remark>
+        /// </summary>
+        public static int PoolSize = DEFAULT_POOL_SIZE;
+
+        private GameObject _poolContainer;
+
+        private Stack<T> _poolStack;
+
+        private static ObjectPool<T> _instance; 
+        public static ObjectPool<T> Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new ObjectPool<T>();
+
+                return _instance;
+            }
+        }
+
+        private ObjectPool()
+        {
+            _poolStack = new Stack<T>(PoolSize);
+            _poolContainer = new GameObject(typeof(T).Name);
+            Transform rootTransform = _poolContainer.transform;
+
+            for (int i = 0; i < PoolSize; i++)
+            {
+                GameObject gameObject = new GameObject();
+                _poolStack.Push(gameObject.AddComponent<T>());
+                gameObject.SetActive(false);
+                gameObject.transform.SetParent(rootTransform);
+            }
+        }
+
+        /// <summary>
+        /// Get an object of type T from the pool
+        /// </summary>
+        /// <returns>an active object of type T</returns>
+        public T AcquireObject()
+        {
+            T item =_poolStack.Pop();
+            item.gameObject.SetActive(true);
+            return item;
+        }
+
+        /// <summary>
+        /// Release an object of type T to the pool
+        /// </summary>
+        /// <param name="objectToRelease"></param>
+        public void ReleaseObject(T objectToRelease)
+        {
+            objectToRelease.gameObject.SetActive(false);
+            objectToRelease.transform.SetParent(_poolContainer.transform);
+            _poolStack.Push(objectToRelease);
+        }
+
+        /// <summary>
+        /// Clear the pool
+        /// </summary>
+        public void Clear()
+        {
+            Object.DestroyImmediate(_poolContainer);
+            _poolStack.Clear();
+            _instance = null;
+        }
+    }
+
+}
---
+++ 1942/Assets/Scripts/Editor/Tests/ObjectPoolTest.cs
@@ -0,0 +1,64 @@
+﻿namespace com.emad.game.tests
+{
+    using UnityEngine;
+    using NUnit.Framework;
+
+    public class ObjectPoolTest
+    {
+        private ObjectPool<Type1> _type1Pool;
+
+        private class Type1 : MonoBehaviour { }
+        private class Type2 : MonoBehaviour { }
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void InstanceTest()
+        {
+            _type1Pool = ObjectPool<Type1>.Instance;
+            Assert.IsNotNull(_type1Pool);
+            Assert.AreEqual(_type1Pool, ObjectPool<Type1>.Instance);
+
+            ObjectPool<Type2> _type2Pool = ObjectPool<Type2>.Instance;
+            Assert.AreNotEqual(_type1Pool, _type2Pool);
+        }
+
+        [Test]
+        public void PoolSizeTest()
+        {
+            ObjectPool<Type1>.PoolSize = 20;
+            Assert.AreEqual(20, ObjectPool<Type1>.PoolSize);
+            _type1Pool = ObjectPool<Type1>.Instance;
+            Assert.AreEqual(20, ObjectPool<Type1>.PoolSize);
+        }
+
+        [Test]
+        public void AcquireObjectTest()
+        {
+            _type1Pool = ObjectPool<Type1>.Instance;
+            Type1 type1Object = _type1Pool.AcquireObject();
+            Assert.IsNotNull(type1Object);
+            Assert.IsTrue(type1Object.GetType() == typeof(Type1));
+        }
+
+        [Test]
+        public void ReleaseObjectTest()
+        {
+            _type1Pool = ObjectPool<Type1>.Instance;
+            Type1 type1Object = _type1Pool.AcquireObject();
+            _type1Pool.ReleaseObject(type1Object);
+            Assert.AreEqual(false, type1Object.gameObject.activeInHierarchy);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _type1Pool.Clear();
+        }
+    }
+
+}
